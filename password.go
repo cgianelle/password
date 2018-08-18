1b256a930bcda323b65f487745ee5a37ec767d94
Chris Gianelle <gianelc@cisco.com>
2018-08-18 19:12:08 -0400

    Fixed Exported/Unexported Identifiers issue with packaging

--- password.go
+++ password.go
@@ -1,11 +1,7 @@
-package main
+package password
 
 import (
-	"fmt"
 	"math/rand"
-	"os"
-	"strconv"
-	"time"
 )
 
 // PasswordInterface Defines the interface that the password generators must honor
@@ -15,18 +11,18 @@ type PasswordInterface interface {
 
 // Password This is the alpha password generator
 type Password struct {
-	characters string
-	r1         *rand.Rand
+	Characters string
+	R1         *rand.Rand
 }
 
 // GenerateCharacter implements teh Password Interface contract and returns a random character as part of the overall password
 func (p Password) GenerateCharacter() string {
-	alphaLen := len(p.characters)
-	index := p.r1.Intn(alphaLen)
-	return string(p.characters[index])
+	alphaLen := len(p.Characters)
+	index := p.R1.Intn(alphaLen)
+	return string(p.Characters[index])
 }
 
-func passwordBuilder(length int, r1 *rand.Rand) func(pwds ...PasswordInterface) string {
+func PasswordBuilder(length int, r1 *rand.Rand) func(pwds ...PasswordInterface) string {
 	return func(pwds ...PasswordInterface) string {
 		var password string
 		for i := 0; i < length; i++ {
@@ -37,27 +33,27 @@ func passwordBuilder(length int, r1 *rand.Rand) func(pwds ...PasswordInterface) 
 	}
 }
 
-func main() {
-	s1 := rand.NewSource(time.Now().UnixNano())
-	r1 := rand.New(s1)
-	var passwdLen, _ = strconv.Atoi(os.Args[1])
-	var passwdType = os.Args[2]
-	var password string
+// func main() {
+// 	s1 := rand.NewSource(time.Now().UnixNano())
+// 	r1 := rand.New(s1)
+// 	var passwdLen, _ = strconv.Atoi(os.Args[1])
+// 	var passwdType = os.Args[2]
+// 	var password string
 
-	alphaPassword := Password{"aAbBcCdDeEfFgGhHiIjJkKlLmMnNoOpPqQrRsStTuUvVwWxXyYzZ", r1}
-	numericPassword := Password{"0123456789", r1}
-	specialPassword := Password{"!@#$%^&*()-_+=,.?/:;'<>?/", r1}
+// 	alphaPassword := Password{"aAbBcCdDeEfFgGhHiIjJkKlLmMnNoOpPqQrRsStTuUvVwWxXyYzZ", r1}
+// 	numericPassword := Password{"0123456789", r1}
+// 	specialPassword := Password{"!@#$%^&*()-_+=,.?/:;'<>?/", r1}
 
-	createPassword := passwordBuilder(passwdLen, r1)
+// 	createPassword := passwordBuilder(passwdLen, r1)
 
-	switch passwdType {
-	case "alpha":
-		password = createPassword(alphaPassword)
-	case "alphaNum":
-		password = createPassword(alphaPassword, numericPassword)
-	case "special":
-		password = createPassword(alphaPassword, numericPassword, specialPassword)
-	}
+// 	switch passwdType {
+// 	case "alpha":
+// 		password = createPassword(alphaPassword)
+// 	case "alphaNum":
+// 		password = createPassword(alphaPassword, numericPassword)
+// 	case "special":
+// 		password = createPassword(alphaPassword, numericPassword, specialPassword)
+// 	}
 
-	fmt.Println(password)
-}
+// 	fmt.Println(password)
+// }
